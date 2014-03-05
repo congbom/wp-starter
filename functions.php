511cd6db1b7d75cc2e2ff1c2f472fef2325984c0
Cong Bom <advcong@gmail.com>
2014-03-05 01:20:57 -0800

    Update scripts

--- functions.php
+++ functions.php
@@ -5,8 +5,8 @@ add_action( 'wp_enqueue_scripts', 'enqueue_scripts_styles' );
 function enqueue_scripts_styles() {
 	wp_enqueue_style( 'styles', get_stylesheet_uri() );
     wp_enqueue_script( 'jquery' );
-	wp_enqueue_script( 'addons', get_template_directory_uri() . '/scripts/addons.js', array(), '1.0', true );
-	wp_enqueue_script( 'scripts', get_template_directory_uri() . '/scripts/scripts.js', array(), '1.0', true );
+	wp_enqueue_script( 'addons', get_template_directory_uri() . '/scripts/addons.js', array(), '1.0' );
+	wp_enqueue_script( 'scripts', get_template_directory_uri() . '/scripts/scripts.js', array(), '1.0' );
 }
 
 
