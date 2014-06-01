8268015d9ed7048f0e43a6c344c78aaabb442852
Cong Bom <advcong@gmail.com>
2014-06-01 13:54:56 +0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -17,6 +17,64 @@ function enqueue_scripts_styles() {
 }
 
 
+// /* Register Menus */
+// register_nav_menus(
+    // array(
+        // 'head-menu' => 'Head Menu',
+        // 'foot-menu' => 'Foot Menu',
+    // )
+// );
 
 
-?>
+// /* Include partials */
+// include_once(get_template_directory() . '/foldername/finename.php');
+
+
+// /* Thumbnail */
+// if ( function_exists( 'add_theme_support' ) ) {
+	// add_theme_support( 'post-thumbnails' );  
+    // add_image_size('thumbnail', 220, 220, true );
+	// add_image_size('small', 300, 200, true );
+	// add_image_size('medium', 768, 0, true );
+	// add_image_size('large', 1024, 0, true );
+// }
+
+// /* Load theme textdomain */
+// add_action ('init', 'theme_init');
+// function theme_init(){
+	// load_theme_textdomain('mb', get_template_directory() . '/resources/lang');
+// }
+
+
+// /* admin_menu hooks */
+// add_action( 'admin_menu', 'custom_admin_menu_hooks' );
+// function custom_admin_menu_hooks() {
+    // /* Change Posts menu label */
+    // global $menu;
+    // $menu[5][0] = 'News';
+
+    // /* Remove 'Comments' menu page */
+    // remove_menu_page( 'edit-comments.php' );  
+// }
+
+
+// /* Custom EXCERPT length */
+// add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
+// function custom_excerpt_length( $length ) {
+    // return 50;
+// }
+
+
+// /* Custom EXCERPT more */
+// add_filter('excerpt_more', 'new_excerpt_more');
+// function new_excerpt_more( $more ) {
+    // return ' ...';
+// }
+
+
+// /* Remove AdminBar */
+// add_filter('show_admin_bar', '__return_false');
+
+
+// /* Remove the_excerpt auto <p> */
+// remove_filter('the_excerpt', 'wpautop');
